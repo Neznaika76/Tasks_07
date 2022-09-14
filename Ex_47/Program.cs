d4409c54872f2680f0792ab16653459eedcbe755
Pavel <n9032692530@gmail.com>
2022-09-14 15:11:42 +0300

    47

---
+++ Ex_47/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+// заполние двумерного массива
+void FillingTheArray(float[,] Array)
+    {
+        Random random = new Random();
+        for(int i=0; i<Array.GetLength(0); i++)
+            for(int j=0; j<Array.GetLength(1); j++)
+                Array[i,j]=random.Next(-100,100);
+        return;
+    }
+
+// Вывод массива
+void OutputTheArray(float[,] Array)
+    {
+        for(int i=0; i<Array.GetLength(0); i++)
+            {
+                for(int j=0; j<Array.GetLength(1); j++)
+                    Console.Write("[{0}]",string.Join(",",Array[i,j]));
+                Console.WriteLine("");
+            }
+        return;
+    }
+
+// Программа
+Console.WriteLine("Введите значение m - ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение n - ");
+int n = Convert.ToInt32(Console.ReadLine());
+float[,] array = new float[m,n];
+FillingTheArray(array);
+OutputTheArray(array);
